893815fb24b188c6d98bb37ad2f26e3f2655e2c6
GabryelJ <gabryeljuliods@gmail.com>
2025-08-08 22:25:33 -0300

    Setup

---
+++ src/main/java/dev/gabryel/LiterAlura/LiterAluraApplication.java
@@ -0,0 +1,25 @@
+package dev.gabryel.LiterAlura;
+
+import dev.gabryel.LiterAlura.main.Main;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	private Main main;
+
+	public LiterAluraApplication(Main main) {
+		this.main = main;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		main.exibirMenu();
+	}
+}
---
+++ src/main/java/dev/gabryel/LiterAlura/main/Main.java
@@ -0,0 +1,21 @@
+package dev.gabryel.LiterAlura.main;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Main {
+
+    public void exibirMenu() {
+        var menu = """
+                Escolha o número da sua opção: 
+                
+                1 - Buscar livro pelo titulo;
+                2 - Listar livros registrados;
+                3 - Listar autores registrados;
+                4 - Listar autores vivos em um dado ano;
+                5 - Listar livros em um determinado idioma;
+                
+                0 - Sair.
+                """
+    }
+}
